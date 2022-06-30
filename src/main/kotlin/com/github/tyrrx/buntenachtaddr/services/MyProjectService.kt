d4cb0a653c0539efd2cde01a57aac62e3691eede
GitHub Action <action@github.com>
2022-06-30 12:21:17 +0000

    Template cleanup

--- src/main/kotlin/com/github/tyrrx/buntenachtaddr/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tyrrx/buntenachtaddr/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tyrrx.buntenachtaddr.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tyrrx.buntenachtaddr.MyBundle
 
 class MyProjectService(project: Project) {
 
--- src/test/kotlin/com/github/tyrrx/buntenachtaddr/MyPluginTest.kt
+++ src/test/kotlin/com/github/tyrrx/buntenachtaddr/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.tyrrx.buntenachtaddr
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
